5797d73bb2d48ae57d4b0dd91ec770536c9274b9
d3lliott <d3elliott2@gmail.com>
2021-01-26 17:40:55 -0500

    pushed

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Syntax_Sugar_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////for the exercise, write the following with proper syntax sugar
+            //int answer = 4;
+            //string response;
+            //if (answer<9)
+            //{
+            //    response = answer + "is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+            //write code below
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+        }
+           
+    }
+}
